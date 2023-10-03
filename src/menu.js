cb19e72de3219563f6db2b668744c8d6e163a8af
Shannqa <116514532+Shannqa@users.noreply.github.com>
2023-10-03 23:03:42 +0200

    Fix 4.

--- src/menu.js
+++ src/menu.js
@@ -1,34 +1,151 @@
-// Menu
-let breakfast = [];
-let starters = [];
-let mains = [];
-// Breakfast
-class Food {
-  constructor(name, price) {
+let menu = [];
+
+class newCategory {
+  constructor(category) {
+    this.category = category;
+    this.dishes = [];
+    menu.push(this);
+  }
+}
+
+class newDish {
+  constructor(name, price, category, description) {
     this.name = name;
     this.price = price;
+    this.category = category;
+    this.description = description;
+    const menuCategory = menu.findIndex(
+      (item) => item.category == this.category
+    );
+    menu[menuCategory].dishes.push(this);
+    //menu.menuCategory.push(this);
   }
 }
 
-class BreakfastItem extends Food {
-  constructor(name, price) {
-    super(name, price);
-    breakfast.push(this);
-  }
-}
 
+/* Categories */
+let starters = new newCategory("Starters");
+let mains = new newCategory("Main Dishes");
+let sides = new newCategory("Sides");
+let desserts = new newCategory("Desserts");
+let drinks = new newCategory("Drinks");
+
+/* Dishes */
+const fishChips = new newDish("Fish and chips", "12.80", "Starters");
+const tunaBowl = new newDish("Tuna bowl", "11.20", "Starters");
+const prosciutto = new newDish(
+  "Prosciutto wrapped mozzarella",
+  "10.40",
+  "Starters"
+);
+const shrimp = new newDish("Shrimp cocktail", "12.10", "Starters");
+
+const sirloin = new newDish(
+  "Classic sirloin",
+  "28.20",
+  "Main Dishes",
+  "with creamed spinach"
+);
+const chickenCass = new newDish(
+  "Spicy chicken casserole",
+  "25.30",
+  "Main Dishes",
+  "with sweet potatoes and baby carrots"
+);
+const spaghetti = new newDish(
+  "Spaghetti",
+  "24.10",
+  "Main Dishes",
+  "with meatballs"
+);
+const salmon = new newDish(
+  "Grilled salmon",
+  "30.90",
+  "Main Dishes",
+  "with fresh asparagus"
+);
+const crabCakes = new newDish(
+  "Crab cakes",
+  "27.30",
+  "Main Dishes",
+  "with tomato salad"
+);
+
+const onionRings = new newDish("Onion rings", "9.20", "Sides");
+const bakesPotatoes = new newDish("Baked potatoes", "7.60", "Sides");
+const kale = new newDish("Kale salad", "8.90", "Sides");
+const ceasar = new newDish("Ceasar salad", "7.60", "Sides");
+const tomatoSoup = new newDish("Tomato soup", "5.90", "Sides");
+const zucchiniSoup = new newDish("Zucchini soup", "6.50", "Sides");
+
+const chocolateCake = new newDish("Chocolate cake", "10.20", "Desserts");
+const carrotCake = new newDish("Carrot cake", "12.10", "Desserts");
+const iceCream = new newDish("Ice Cream Explosion", "9.60", "Desserts");
+const limeMousse = new newDish("Lime Mousse", "10.80", "Desserts");
+const peachCrumble = new newDish("Peach Crumble", "11.20", "Desserts");
+const cheesecake = new newDish("Blackberry Cheesecake", "12.80", "Desserts");
+
+const wine = new newDish(
+  "Glass of wine",
+  "12.60",
+  "Drinks",
+  "sparkling, rosé, red, white"
+);
+const beer = new newDish("Beer", "8.30", "Drinks", "lager, ale, cider");
+const soda = new newDish("Soda", "5.80", "Drinks", "coke, pepsi, fanta");
+const water = new newDish("Water", "no charge", "Drinks", "sparkling, still");
+
+
+// Create menu on page 
 const createMenu = function () {
   const contentDiv = document.querySelector("#content");
+
+  // remove content from a previous page
   while (contentDiv.firstChild) {
     contentDiv.removeChild(contentDiv.lastChild);
   }
-};
-const pancakes = new BreakfastItem("Pancakes", "8.99");
+  contentDiv.className = "";
+  contentDiv.classList.add("content-menu");
+
+  // add page title and container
+  const menuH1 = document.createElement("h1");
+  menuH1.textContent = "Menu";
+  contentDiv.appendChild(menuH1);
+  const menuContainer = document.createElement("div");
+  menuContainer.classList.add("menu-container");
+  contentDiv.appendChild(menuContainer);
+
+  // fill the menu with categories
+  menu.forEach((item) => {
+    const categoryDiv = document.createElement("div");
+    categoryDiv.classList.add("menu-category");
+    const categoryH2 = document.createElement("h2");
+    categoryH2.textContent = item.category;
+    categoryDiv.appendChild(categoryH2);
+    menuContainer.appendChild(categoryDiv);
+
+    // fill the categories with dishes
+    function fillCategories() {
+      item.dishes.forEach((dish) => {
+        console.log(dish.name);
+        const dishDiv = document.createElement("div");
+        dishDiv.classList.add("menu-dish");
+        const dishName = document.createElement("div");
+        const dishPrice = document.createElement("div");
+        const dishDescription = document.createElement("div");
+        dishName.textContent = dish.name;
+        dishPrice.textContent = dish.price;
+        dishDescription.textContent = dish.description;
+
+        dishDiv.appendChild(dishName);
+        dishDiv.appendChild(dishPrice);
+        dishDiv.appendChild(dishDescription);
+        categoryDiv.appendChild(dishDiv);
+      });
+    }
+    fillCategories();
+  });
+}
+
 export default createMenu;
-// Starters
 
-// Main Dishes
-
-// Desserts
-
-// Beverages
